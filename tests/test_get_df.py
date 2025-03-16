2536d5c7b95abbf10f0a9acb24a4112d55472727
Sebastián Dalceggio <sdalceggio@gmail.com>
2025-03-16 11:04:13 -0300

    Configuration of Ruff

--- tests/test_get_df.py
+++ tests/test_get_df.py
@@ -1,35 +1,31 @@
-"Test module"
+"""Test module."""
 
 import pandas as pd
 
 from python_package.main import get_df
 
+
 def test_get_df_correct_shape():
-    """
-    Tests if the DataFrame created by get_df has the correct shape (rows, columns).
-    """
-    df = get_df(5)
-    assert df.shape == (5, 2)
+    """Tests if the DataFrame created by get_df has the correct shape (rows, columns)."""
+    numbers_df = get_df(5)
+    assert numbers_df.shape == (5, 2)
+
 
 def test_get_df_correct_values():
-    """
-    Tests if the DataFrame created by get_df contains the correct values.
-    """
-    df = get_df(3)
+    """Tests if the DataFrame created by get_df contains the correct values."""
+    numbers_df = get_df(3)
     expected_data = [[0, 0], [1, 1], [2, 2]]
     expected_df = pd.DataFrame(expected_data, columns=["A", "B"])
-    pd.testing.assert_frame_equal(df, expected_df)
+    pd.testing.assert_frame_equal(numbers_df, expected_df)
+
 
 def test_get_df_empty():
-    """
-    Tests if get_df returns an empty DataFrame when rows is 0.
-    """
-    df = get_df(0)
-    assert df.empty
+    """Tests if get_df returns an empty DataFrame when rows is 0."""
+    numbers_df = get_df(0)
+    assert numbers_df.empty
+
 
 def test_get_df_correct_columns():
-    """
-    Tests if the DataFrame created by get_df has the correct column names.
-    """
-    df = get_df(1)
-    assert list(df.columns) == ["A", "B"]
+    """Tests if the DataFrame created by get_df has the correct column names."""
+    numbers_df = get_df(1)
+    assert list(numbers_df.columns) == ["A", "B"]
